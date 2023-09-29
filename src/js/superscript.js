18c77a76051bbb8214d96a473bc494d7b62f8089
Melody <cur21014@byui.edu>
2023-09-28 18:01:05 -0600

    Added superscript to show the number of items in the cart

---
+++ src/js/superscript.js
@@ -0,0 +1,13 @@
+import { getLocalStorage } from "./utils.mjs";
+
+function cartItemSuperscript() {
+  let cartItems = getLocalStorage("so-cart");
+
+  if (cartItems.length > 0) {
+    let superscriptContainer = document.createElement("span");
+    document.querySelector(".cart").append(superscriptContainer);
+    superscriptContainer.textContent = cartItems.length;
+  }
+}
+
+cartItemSuperscript();
