371b4241e31a0e72f19915f8b5e8fe89ddf085b3
Melody <cur21014@byui.edu>
2023-11-25 17:18:08 -0700

    Fixed formatting issue

--- src/js/login.js
+++ src/js/login.js
@@ -1,24 +1,23 @@
 import { loadHeaderFooter } from "./utils.mjs";
 import cartItemSuperscript from "./superscript";
-import { getParam } from "./utils.mjs";
 import { login } from "./auth.mjs";
-import { checkLogin } from "./auth.mjs";
-import { check } from "prettier";
 
-await loadHeaderFooter();
-cartItemSuperscript();
+main();
+async function main() {
+  await loadHeaderFooter();
+  cartItemSuperscript();
+}
 
 // const redirect = getParam("redirect");
 
-let loginBtn = document.querySelector('#loginBtn');
+let loginBtn = document.querySelector("#loginBtn");
 let creds;
 
 loginBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    let email = document.querySelector('#email').value;
-    let password = document.querySelector('#password').value;
+  e.preventDefault();
+  let email = document.querySelector("#email").value;
+  let password = document.querySelector("#password").value;
 
-    creds = {email, password};
-    login(creds);
-})
-
+  creds = { email, password };
+  login(creds);
+});
--- src/js/orders.js
+++ src/js/orders.js
@@ -2,10 +2,10 @@ import cartItemSuperscript from "./superscript";
 import { loadHeaderFooter } from "./utils.mjs";
 import { orderDisplay } from "./currentOrders.mjs";
 
-
-await loadHeaderFooter();
-cartItemSuperscript();
+main();
+async function main() {
+  await loadHeaderFooter();
+  cartItemSuperscript();
+}
 
 orderDisplay();
-
-
